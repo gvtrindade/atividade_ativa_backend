5579f251d630403968e76fa49d5fa6a6874df229
Gabriel Trindade <gabriel.trindade@basis.com.br>
2022-11-28 14:01:34 -0300

    Telas criadas

--- atividadeativa/backend/views.py
+++ atividadeativa/backend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(req):
+    return render(req, 'backend/index.html')
+
+def results(req):
+    return render(req, 'backend/results.html')
+
+def vote(req):
+    return render(req, 'backend/vote.html')
